aee962133c944e8f8edeec9d5e171dbd63b8bfc0
Anderson <113170554+cactum@users.noreply.github.com>
2023-07-03 19:42:34 -0300

    add program conversor de moedas

--- Conversor de Moedas/Program.cs
+++ Conversor de Moedas/Program.cs
@@ -5,84 +5,81 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConversorMoedas
+class Conversor
 {
-    internal class Program
+    private float cotacaoMoeda;   
+
+    public void DefinirCotacaoMoeda(string opMoeda)
     {
-        static void Main()
+        Console.Clear();
+        Console.WriteLine("Digite a cotação atual:");
+        string input = Console.ReadLine();
+        if (float.TryParse(input, out float cotacao))
         {
-            Conversor conversor = new Conversor();
+            cotacaoMoeda = cotacao;
+            Console.WriteLine($"Cotacao do {opMoeda} definida com sucesso!");            
 
-            Console.WriteLine("Escolha para qual moeda deseja converter:\n1-Dolar\n2-Euro");
-            int op;
-            if (int.TryParse(Console.ReadLine(), out op))
-            {
-                if (op >= 1 && op <= 2)
-                {
-                    conversor.DefinirCotacao(op);
-                }
-                else
-                {
-                    Console.WriteLine("Opção Invalida");
-                    Console.ReadKey();
-                    Main();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
-                Console.ReadKey();
-                Main();
-            }
-
-            Console.Write("Digite um valor em Real (BRL): ");
-            float valorEmReal = float.Parse(Console.ReadLine());
-
-            float valorEmDolar = conversor.ConverterRealParaDolar(valorEmReal);
-
-            Console.WriteLine("Valor em Dólar (USD): " + valorEmDolar);
         }
-
-
-
-        class Conversor
+        else
         {
-            public float Dolar;
-            public float Euro;
-            private float Moeda;
+            Console.WriteLine($"Valor invalido. A cotação do {opMoeda} não foi definida.");
+            DefinirCotacaoMoeda(opMoeda);
 
-            public void DefinirCotacao(int opcao)
-            {
-                /*
-                Console.Write("Digite a cotação atual do Dólar: ");
-                string input = Console.ReadLine();
-                if (float.TryParse(input, out float cotacao))
-                {
-                    Moeda = cotacao;
-                    Console.WriteLine("Cotação da moeda definida com sucesso!");
-                }
-                else
-                {
-                    Console.WriteLine("Valor inválido. A cotação não foi definida.");
-                }
-                 */
-
-            }
-
-            public float ConverterRealParaDolar(float Real)
-            {
-                return Real / Dolar;
-            }
-
-            public float ConverterRealParaEuro(float Real)
-            {
-                return Real / Euro;
-            }
         }
-
-
     }
+
+
+    public float ConverterRealEmMoeda(float ValorEmReal)
+    {
+        return ValorEmReal / cotacaoMoeda;
+    }
+
 
 }
 
+class program
+{
+    static void Main()
+    {
+        string Moeda = "";
+        int opcao;
+        Conversor conversor = new Conversor();
+        
+        Console.WriteLine("Escolha a opção para qual deseja converter:");
+        Console.WriteLine("1-Dolar\n2-Euro");
+        opcao = int.Parse(Console.ReadLine());
 
+        if (opcao == 1)
+        {
+            Moeda = "Dolar (USD)";
+            conversor.DefinirCotacaoMoeda(Moeda);
+        } 
+        else if (opcao == 2)
+        {
+            Moeda = "Euro (EUR)";
+            conversor.DefinirCotacaoMoeda(Moeda);
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+            Main();
+        }
+
+        Console.WriteLine("Digite o valor em Real (BRL): ");
+        float ValorEmReal = float.Parse(Console.ReadLine());
+
+        int opConversao = opcao;
+
+        if (opConversao == 1 || opConversao == 2)
+        {
+            float ValorConvertido = conversor.ConverterRealEmMoeda(ValorEmReal);
+            Console.WriteLine($"valor em {Moeda}: " + ValorConvertido);
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida. Encerrando o programa.");
+            return;
+        }
+    }
+}
