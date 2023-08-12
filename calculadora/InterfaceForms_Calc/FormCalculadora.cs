e726e607f0ca685b1d659c927cd3054f6babafc0
Anderson <113170554+cactum@users.noreply.github.com>
2023-08-12 18:36:15 -0300

    add interface winForms
    add projeto de interface da calculadora

---
+++ calculadora/InterfaceForms_Calc/FormCalculadora.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AprendendoWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tela_calculadora(object sender, EventArgs e)
+        {
+            
+        }
+        private void buttom_CE(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_C(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_BACKSPACE(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_DIVISAO(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_7(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button_8(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_9(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MULTIPLICACAO(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_SUBTRACAO(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ADICAO(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_0(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_VIRGULA(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_RESULTADO(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
